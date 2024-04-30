149ddc7a773a2efb363c2969f72e3d52b01bc45a
Fırat Kazak <firatkazak@gmail.com>
2024-04-30 14:25:47 +0300

    Last try for docker compose.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,14 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : ICommand<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session)
-    : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         //create Product entity from command object
-        //save to database
-        //return CreateProductResult result
-
         var product = new Product
         {
             Name = command.Name,
@@ -21,11 +17,9 @@ internal class CreateProductCommandHandler(IDocumentSession session)
             ImageFile = command.ImageFile,
             Price = command.Price
         };
-
         //save to database
         session.Store(product);
         await session.SaveChangesAsync(cancellationToken);
-
         //return result
         return new CreateProductResult(product.Id);
     }
