b163cdadb1e626328ea3f6f73857480e665618df
Fırat Kazak <firatkazak@gmail.com>
2024-04-30 14:21:55 +0300

    Docker Compose Problem.

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,9 @@
+﻿// Packages;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Marten;
+// Project references;
+global using BuildingBlocks.CQRS;
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
